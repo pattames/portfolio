a8f7f4bf531d62e0d312acfceb3fb59d0b497cf4
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-05-18 16:27:41 +0200

    first classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <div className="bg-indigo-600 min-h-screen"></div>
-    </div>
+    <main>
+      <section className="bg-slate-500 min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1>Patricio Tamés</h1>
+          <ul>
+            <li>
+              <BsFillMoonStarsFill />
+            </li>
+            <li>
+              <a href="#">Linkedln</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </main>
   );
 }
-
-export default App;
