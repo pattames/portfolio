d690654d47a81391707484f1bd45d41754354901
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-05-22 13:08:06 +0200

    almoast done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { useState } from "react";
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   //Aquí mis proyectos:
 
@@ -12,9 +12,9 @@ export default function App() {
     <div className={darkMode ? "dark" : ""}>
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section>
-          <nav className="py-10 mb-12 flex justify-between opacity-0 animate-slideInFromTop">
-            <h1 className="text-xl font-burtons dark:text-white duration-200 hover:scale-110">
-              Patricio Tamés
+          <nav className="py-10 mb-12 flex justify-between opacity-0 animate-slideInFromTop rounded-xl">
+            <h1 className="text-2xl dark:text-white font-roboto hover:scale-110 duration-200">
+              Portfolio
             </h1>
             <ul className="flex items-center">
               <li className="animate-bounceOnce mr-8">
@@ -41,11 +41,11 @@ export default function App() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-3 text-teal-500 font-medium md:text-6xl dark:text-amber-500 animate-ping tracking-wider">
+            <h2 className="text-6xl py-3 text-teal-500 font-extralight md:text-7xl dark:text-amber-500 animate-ping tracking-wider font-roboto">
               Patricio Tamés
             </h2>
             <h3 className="text-2xl py-3 md:text-3xl dark:text-white opacity-0 animate-fade">
-              Junior Full-Stack Developer.
+              Web Developer.
             </h3>
             <p className="text-md py-6 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-400 opacity-0 animate-fade">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
@@ -58,7 +58,7 @@ export default function App() {
             <AiFillLinkedin className="duration-200 hover:scale-125" />
             <AiFillInstagram className="duration-200 hover:scale-125" />
           </div>
-          <div className="mt-20 mb-10 flex justify-center opacity-0 animate-fade">
+          <div className="mt-20 mb-10 flex justify-center opacity-0 animate-fade shadow-2xl shadow-gray-400 rounded-full max-w-96 mx-auto dark:shadow-none">
             <img
               className="rounded-full w-96"
               src="profilepic.png"
@@ -86,7 +86,7 @@ export default function App() {
             </p>
           </div>
           <div className="lg:flex gap-10 justify-center opacity-0 animate-fade">
-            <div className="text-center shadow-lg shadow-gray-400 p-10 rounded-xl my-10 flex-1 dark:shadow-slate-600 duration-200 hover:scale-105">
+            <div className="text-center shadow-xl shadow-gray-300 p-10 rounded-xl my-10 flex-1 dark:shadow-none duration-200 hover:scale-105 dark:bg-gray-800">
               <img
                 className="mx-auto"
                 src="design.png"
@@ -108,7 +108,7 @@ export default function App() {
               </p>
               <p className="text-gray-800 py-1 dark:text-gray-400">Figma</p>
             </div>
-            <div className="text-center shadow-lg shadow-gray-400 p-10 rounded-xl my-10 flex-1 dark:shadow-slate-600 duration-200 hover:scale-105">
+            <div className="text-center shadow-xl shadow-gray-300 p-10 rounded-xl my-10 flex-1 dark:shadow-none duration-200 hover:scale-105 dark:bg-gray-800">
               <img
                 className="mx-auto"
                 src="code.png"
@@ -130,7 +130,7 @@ export default function App() {
               </p>
               <p className="text-gray-800 py-1 dark:text-gray-400">Figma</p>
             </div>
-            <div className="text-center shadow-lg shadow-gray-400 p-10 rounded-xl my-10 flex-1 dark:shadow-slate-600 duration-200 hover:scale-105">
+            <div className="text-center shadow-xl shadow-gray-300 p-10 rounded-xl my-10 flex-1 dark:shadow-none duration-200 hover:scale-105 dark:bg-gray-800">
               <img
                 className="mx-auto"
                 src="consulting.png"
@@ -174,9 +174,9 @@ export default function App() {
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="rounded-xl group basis-1/3 flex-1 duration-200 hover:scale-105 shadow-lg shadow-gray-400">
-              <h1 className=" bg-black p-2 bg-opacity-40 rounded-md text-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-200 text-white z-10">
-                Website
+            <div className="rounded-xl group basis-1/3 flex-1 duration-200 hover:scale-105 shadow-2xl shadow-gray-400 dark:shadow-none hover:shadow-none">
+              <h1 className="bg-white p-3 bg-opacity-60 rounded-md text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-200 text-black z-10 tracking-wide">
+                Petfect Match
               </h1>
               <img
                 className="rounded-xl object-cover group-hover:blur-sm duration-200"
