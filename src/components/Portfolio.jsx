17a63f35a5e00db72e1946a3eb27f50185836349
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-05-23 13:25:41 +0200

    portfolio data casi listo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,3 +1,6 @@
+import PortfolioItem from "./PortfolioItem";
+import portfolio from "../data/portfolio";
+
 export default function Portfolio() {
   return (
     <section className="opacity-0 animate-fade">
@@ -18,25 +21,14 @@ export default function Portfolio() {
         </p>
       </div>
       <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-        <div className="rounded-xl group basis-1/3 flex-1 duration-200 hover:scale-105 shadow-2xl shadow-gray-400 dark:shadow-none hover:shadow-none">
-          <h1 className="bg-white p-3 bg-opacity-60 rounded-md text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 duration-200 text-black z-10 tracking-wide">
-            Petfect Match
-          </h1>
-          <img
-            className="rounded-xl object-cover group-hover:blur-sm duration-200"
-            src="web1.png"
-            alt=""
+        {portfolio.map((project) => (
+          <PortfolioItem
+            title={project.title}
+            imgUrl={project.imgUrl}
+            stack={project.stack}
+            link={project.link}
           />
-        </div>
-        <div className="basis-1/3 flex-1 duration-200 hover:scale-105">
-          <img className="rounded-lg object-cover" src="web2.png" alt="" />
-        </div>
-        <div className="basis-1/3 flex-1 duration-200 hover:scale-105">
-          <img className="rounded-lg object-cover" src="web3.png" alt="" />
-        </div>
-        <div className="basis-1/3 flex-1 duration-200 hover:scale-105">
-          <img className="rounded-lg object-cover" src="web4.png" alt="" />
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/data/portfolio.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Prueba1",
+    imgUrl: "web1.png",
+    stack: ["React", "Node.js", "Express", "MongoDB"],
+    link: "https://petfectmatch.netlify.app/",
+  },
+  {
+    title: "Prueba2",
+    imgUrl: "web2.png",
+    stack: ["React", "Node.js", "Express", "MongoDB"],
+    link: "https://petfectmatch.netlify.app/",
+  },
+];
