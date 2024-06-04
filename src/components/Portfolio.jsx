6ff5b7f667f88ddf37e57fefd999e63d5ecf641b
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-06-04 12:40:46 +0200

    opciones de mensajes en portafolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,20 +7,25 @@ export default function Portfolio() {
       <div>
         <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
         <p className="text-md py-2 leading-8 text-gray-500 dark:text-gray-400">
-          All{" "}
-          <span className="text-teal-500 dark:text-amber-500">projects</span>{" "}
-          listed on this site are created from the ground up, starting with
-          basic wireframing and progressing to fully functional{" "}
-          <span className="text-teal-500 dark:text-amber-500">
-            applications
-          </span>
-          .
+          All projects listed on this site are created from the{" "}
+          <span className="text-teal-500 dark:text-amber-500">ground up</span>,
+          starting with basic wireframing and progressing to fully functional
+          applications.
         </p>
+        {/* En caso de no mostrar las tecnologías en las cartas: */}
         <p className="text-md py-2 leading-8 text-gray-500 dark:text-gray-400">
-          Important: Due to the use of a free backend deployment service, the
-          websites take an initial time of 40 seconds to load all of the data
-          before running smoothly.
+          Technologies used: React for the{" "}
+          <span className="text-teal-500 dark:text-amber-500">frontend</span>,
+          and Node.js, Express, and MongoDB for the{" "}
+          <span className="text-teal-500 dark:text-amber-500">backend</span>.
         </p>
+        {/* En caso de no pagar backend deployment: */}
+        {/* <p className="text-md py-2 leading-8 text-gray-500 dark:text-gray-400">
+          <span className="text-teal-500 dark:text-amber-500">Important:</span>{" "}
+          Due to the use of a free backend deployment service, the websites may
+          take approximately 40 seconds to load all data initially before
+          running smoothly.
+        </p> */}
       </div>
       <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
         {portfolio.map((project) => (
