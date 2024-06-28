9fabb9e0f7965d1a73b6858ed03cd346b5e658be
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-06-28 20:25:36 +0200

    alv router dom

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import ThemeContextProvider from "./context/ThemeContext.jsx";
 import LangContextProvider from "./context/LangContext.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
