c630fad7436bf8e5a0bfe788665baa0254b97f51
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-07-31 12:31:20 -0600

    comented out darkmode smooth transition en index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -37,4 +36,5 @@ export default {
     },
   },
   plugins: [],
+  darkMode: "class",
 };
