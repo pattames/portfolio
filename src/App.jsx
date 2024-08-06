c3ea271776d5b38febfca3f30dbaa44b1a333cd4
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-08-05 19:26:24 -0600

    prueba 2 darkmode mobile

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
   // }, [darkMode]);
 
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900">
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <Navbar />
         <Profile />
